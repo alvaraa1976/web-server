628bda5b22fa9c616f32390cc2eec780b7876735
Alex Alvarado <alexander.alvarado@gmail.com>
2016-01-22 14:46:36 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+//application middleware
+var middleware = {
+    requireAuthentication: function (req, res, next) {
+        console.log('private route hit');
+        next();
+    },
+    logger: function (req, res, next) {
+        var timeStamp = new Date().toString();
+        console.log('Request: ' + req.method + ' at ' + timeStamp + ' on ' + req.originalUrl);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication)
+
+//app.get('/', function (req, res) {
+//    res.send('hello express');
+//})
+
+//get takes route and function. Function takes request object and response
+//app.get('/about', function (req, res) {
+//    res.send('About Us');
+//})
+
+//route-specific middleware
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send('About Us');
+})
+
+//expose entire public folder
+app.use(express.static(__dirname + '/public'));
+
+//app.listen(3000);//choose port to listen on
+
+app.listen(PORT, function () {
+    console.log('Express Server started on port ' + PORT);
+});
